b00d34dca13708725ac4345cb6580efc020c4c51
Arpit Nuwal <45119425+arpitnuwal@users.noreply.github.com>
2026-03-28 09:35:39 +0530

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+<!DOCTYPE html>
+<html lang="en">
+<head id="Head1" runat="server">
+    <meta charset="utf-8" />
+    <title>Online Image Slider</title>
+    <!-- Bootstrap CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container mt-5">
+            <asp:Literal ID="litSlider" runat="server"></asp:Literal>
+        </div>
+    </form>
+
+    <!-- Bootstrap JS CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+      protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            string[] imageUrls = new string[]
+            {
+                "https://picsum.photos/id/1011/800/400",
+                "https://picsum.photos/id/1021/800/400",
+                "https://picsum.photos/id/1031/800/400",
+                "https://picsum.photos/id/1041/800/400"
+            };
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(@"
+<div id='carouselExampleIndicators' class='carousel slide' data-bs-ride='carousel'>
+  <div class='carousel-indicators'>");
+
+            for (int i = 0; i < imageUrls.Length; i++)
+            {
+                sb.AppendFormat("<button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='{0}' {1} aria-label='Slide {0}'></button>",
+                    i, i == 0 ? "class='active' aria-current='true'" : "");
+            }
+
+            sb.Append(@"
+  </div>
+  <div class='carousel-inner'>");
+
+            for (int i = 0; i < imageUrls.Length; i++)
+            {
+                sb.AppendFormat(@"<div class='carousel-item {0}'>
+                        <img src='{1}' class='d-block w-100' alt='Slide {2}' />
+                      </div>",
+                    i == 0 ? "active" : "", imageUrls[i], i + 1);
+            }
+
+            sb.Append(@"
+  </div>
+  <button class='carousel-control-prev' type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide='prev'>
+    <span class='carousel-control-prev-icon' aria-hidden='true'></span>
+    <span class='visually-hidden'>Previous</span>
+  </button>
+  <button class='carousel-control-next' type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide='next'>
+    <span class='carousel-control-next-icon' aria-hidden='true'></span>
+    <span class='visually-hidden'>Next</span>
+  </button>
+</div>");
+
+            litSlider.Text = sb.ToString();
+        }
+    }
+
+}
